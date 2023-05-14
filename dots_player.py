aff79d85e1a9be7cd2dd4498f776859dd3b4fcd4
Stepan Kuchin <stepa.ku4in@gmail.com>
2023-05-14 19:47:30 +0500

    add caught_area

--- dots_player.py
+++ dots_player.py
@@ -2,18 +2,22 @@ import pygame.font
 
 BLUE = (0, 0, 139)
 RED = (255, 69, 0)
-PLAYER_COLOURS = {0: BLUE, 1: RED}
+GREEN = (0, 139, 0)
+PLAYER_COLOURS = {0: BLUE, 1: RED, 2: GREEN}
 
 pygame.font.init()
-f = pygame.font.SysFont("arial", 24)
+f = pygame.font.SysFont('arial', 24)
 
 
 class Player:
-    def __init__(self, number):
+    def __init__(self, number, name=None):
         self.number = number
         self.pressed_dots = set()
         self.colour = PLAYER_COLOURS[number]
         self.points = 0
+        self.name = name
+        if name is None:
+            self.name = f'Player{self.number}'
 
     def get_points_text(self):
-        return f.render(f"Player{self.number}: {self.points}", True, self.colour)
+        return f.render(f'{self.name}: {self.points}', True, self.colour)
--- game.py
+++ game.py
@@ -17,7 +17,6 @@ pygame.init()
 class Game:
     def __init__(self, column_count, line_count, players_count):
         self._dots = set()
-        # количество квадратов по горизонтали и вертикали
         self._map = game_map.Map(WIDTH, HEIGHT, column_count, line_count)
         self._steps_count = (column_count + 1) * (line_count + 1)
         self._players = []
@@ -26,6 +25,7 @@ class Game:
             player = dots_player.Player(i)
             self._players.append(player)
         self.current_player = 0
+        self._caught_dots = set()
 
     def _create_start_screen(self):
         self._screen = pygame.display.set_mode(SIZE)
@@ -34,48 +34,46 @@ class Game:
             pygame.draw.rect(self._screen, GRAY, rect, 1)
         for i in range(len(self._players)):
             self._screen.blit(self._players[i].get_points_text(), (10, 10 + i * 30))
-            #edge = 10
-            #if i % 2:
-            #    edge = 450
-            #self._screen.blit(self._players[i].get_points_text(), (edge, 10 + i // 2 * 30))
         pygame.display.flip()
 
     def _update_player_index(self):
         self.current_player += 1
         self.current_player %= self.players_count
 
-    def _is_not_used_point(self, dot):
+    def _is_used_point(self, dot):
         for player in self._players:
             if dot in player.pressed_dots:
-                return False
-        return True
-
-    def _try_make_step(self, pos):
-        player = self._players[self.current_player]
-        colour = player.colour
-        if self._is_not_used_point(pos):
-            pygame.draw.circle(self._screen, colour, pos, 5)
-            pygame.display.update()
-            player.pressed_dots.add(pos)
-            self._try_find_cycle(pos)
-            self._update_player_index()
-            return True
+                return True
         return False
 
-    def _try_find_cycle(self, pos):
+    def _try_make_step(self, pos):
+        if pos in self._caught_dots:
+            return False
+        if self._is_used_point(pos):
+            return False
         player = self._players[self.current_player]
-        cycles = bfs.find_cycles(pos, player.pressed_dots)
+        colour = player.colour
+        pygame.draw.circle(self._screen, colour, pos, 5)
+        pygame.display.update()
+        player.pressed_dots.add(pos)
+        self._try_find_catching_cycle(pos)
+        self._update_player_index()
+        return True
+
+    def _try_find_catching_cycle(self, pos):
+        player = self._players[self.current_player]
+        non_caught_dots = player.pressed_dots.difference(self._caught_dots)
+        cycles = bfs.find_cycles(pos, non_caught_dots)
         for cycle in cycles:
             path_list = list(cycle)
-            path_set = set(path_list)
-            inside_point = bfs.find_close_point_inside(path_set, pos, game_map.SQUARE_SIZE)
-            if inside_point is None:
-                continue
-            inside_area = bfs.get_inside_area(path_set, inside_point, game_map.SQUARE_SIZE)
+            inside_area = _get_area_inside_cycle(path_list, pos)
+            inside_area = inside_area.difference(self._caught_dots)
             enemy_points = self._intersect_enemy_dots(inside_area, player)
             if len(enemy_points) > 0:
                 self._draw_shape(path_list)
+                self._caught_dots = self._caught_dots.union(inside_area)
                 player.points += len(enemy_points)
+                # НЕОБХОДИМО ДОПИСАТЬ ОБНОВЛЕНИЕ ЭКРАНА С ОЧКАМИ
 
     def _intersect_enemy_dots(self, area_inside_cycle, current_player):
         enemy_points = set()
@@ -107,3 +105,11 @@ class Game:
                             is_success = self._try_make_step(pos)
                             if is_success:
                                 self._steps_count += 1
+
+
+def _get_area_inside_cycle(path, pos):
+    path_set = set(path)
+    inside_point = bfs.find_close_point_inside(path_set, pos, game_map.SQUARE_SIZE)
+    if inside_point is None:
+        return set()
+    return bfs.get_inside_area(path_set, inside_point, game_map.SQUARE_SIZE)
